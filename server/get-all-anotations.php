72ca55b731ecff956ed129564961ee74a99414cb
nurxx <ilknurmustafa99@gmail.com>
2020-06-13 00:54:08 +0300

    Add read from database functionality

---
+++ server/get-all-anotations.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "./src/Anotation.php";
+
+$anotation = new Anotation(0, 0, '');
+$readStatement = $anotation->read();
+$count = $readStatement->rowCount();
+
+if ($count > 0) {
+    $anotations = array();
+    $anotations["result"] = array();
+
+    while ($row = $readStatement->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $item = array(
+            "latitude" => $latitude,
+            "longitude" => $longitude,
+            "tooltip" => $tooltip
+        );
+
+        array_push($anotations["result"], $item);
+    }
+
+    http_response_code(200);
+
+    echo json_encode(["success" => true, "message" => "Found anotations.", "result" => $anotations["result"]]);
+
+    return;
+}
+
+http_response_code(404);
+echo json_encode(["success" => false, "message" => "No itanotations found."]);
