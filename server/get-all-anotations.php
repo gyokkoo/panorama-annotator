bb512049684b13902863ea77fada5e8284be640f
nurxx <ilknurmustafa99@gmail.com>
2020-06-15 22:41:14 +0300

    Fix undefined value if no annotations existing on export

--- server/get-all-anotations.php
+++ server/get-all-anotations.php
@@ -4,33 +4,25 @@ require_once "./src/Anotation.php";
 
 $anotation = new Anotation('', 0, 0, '', '', '');
 $readStatement = $anotation->read();
-$count = $readStatement->rowCount();
 
-if ($count > 0) {
-    $anotations = array();
-    $anotations["result"] = array();
+$anotations = array();
+$anotations["result"] = array();
 
-    while ($row = $readStatement->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
+while ($row = $readStatement->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
 
-        $item = array(
-            "id" => $id,
-            "latitude" => $latitude,
-            "longitude" => $longitude,
-            "tooltip" => $tooltip,
-            "panoramaImage" => $panoramaImage,
-            "anotationImage" => $anotationImage
-        );
+    $item = array(
+        "id" => $id,
+        "latitude" => $latitude,
+        "longitude" => $longitude,
+        "tooltip" => $tooltip,
+        "panoramaImage" => $panoramaImage,
+        "anotationImage" => $anotationImage
+    );
 
-        array_push($anotations["result"], $item);
-    }
-
-    http_response_code(200);
-
-    echo json_encode(["success" => true, "message" => "Found anotations.", "result" => $anotations["result"]]);
-
-    return;
+    array_push($anotations["result"], $item);
 }
 
-http_response_code(404);
-echo json_encode(["success" => false, "message" => "No anotations found."]);
+http_response_code(200);
+
+echo json_encode(["success" => true, "message" => "Found anotations.", "result" => $anotations["result"]]);
