881c3e2298946a39a27ae23530433fa6761f965b
gyokkoo <gyokan.nezhdetov@gmail.com>
2020-07-06 18:14:16 +0300

    Add url link

--- server/config.php
+++ server/config.php
@@ -6,7 +6,7 @@ class Config
     static $SITE_CREATOR = "Gyokan Syuleymanov, Ilknur Mustafa";
     static $SITE_ADMIN_EMAIL = "gyokan.syuleymanov@gmail.com or ilknurmustafa99@gmail.com";
     static $SITE_INFO = "This project was created during 2020 year, on Web Technologies, Sofia University, FMI, lead by: Milen Petrov";
-    static $SITE_URL = ""; // TODO: add link to deployed file.
+    static $SITE_URL = "http://ec2-3-93-172-10.compute-1.amazonaws.com/panorama-annotator/app/index.html";
     static $ROOT_FOLDER = "/panorama-annotator/app/index.html";
     static $SITE_DESCRIPTION = "Create panorama annotations and share memories";
 
